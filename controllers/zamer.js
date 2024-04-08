d0f9ad0b86d4ece1925a0fc2f0eb5c7ef1d597f3
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-04-08 21:48:23 +0300

    alfa 3.6

--- controllers/zamer.js
+++ controllers/zamer.js
@@ -1,4 +1,4 @@
-import lodzi from '../models/model.js';
+import {lodzi, lodziExtensive} from '../models/model.js';
 
 class Zamer{
 
@@ -7,10 +7,10 @@ class Zamer{
      }
      async mongopost(req, res) {
           try {
-               const {name, phone} = req.body;
+               const {name, tel} = req.body;
 
                const isNotNewEl = await lodzi.findOne({
-                    tel: phone
+                    tel: tel
                })
                if (isNotNewEl) {
                     res.status(409).json({
@@ -21,7 +21,41 @@ class Zamer{
 
                const postzamer = new lodzi({
                     name: name,
-                    tel: phone
+                    tel: tel
+               })
+
+               await postzamer.save();
+               res.status(201).json({
+                    message: 'Успех'
+               });
+          
+          }
+          catch(e) {
+               console.log(e, 'ЗАМЕРА не будет');
+          }
+     }
+     async mongopost(req, res) {
+          try {
+               const {name, tel, type, width, height, glass, glass2} = req.body;
+
+               const isNotNewEl = await lodzi.findOne({
+                    tel: tel
+               })
+               if (isNotNewEl) {
+                    res.status(409).json({
+                         message: 'Уже имеется'
+                    })
+                    return;
+               }
+
+               const postzamer = new lodzi({
+                    name: name,
+                    tel: tel,
+                    type: type,
+                    width: width,
+                    height: height,
+                    glass: glass,
+                    glass2: glass2,
                })
 
                await postzamer.save();
--- models/model.js
+++ models/model.js
@@ -13,6 +13,45 @@ const Schema = new mongoose.Schema({
     }
 });
 
-const lodzi  = new mongoose.model('Zamer', Schema);
+const SchemaExtensive = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    tel: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    type: {
+        type: Integer,
+        required: true,
+        unique: false
+    },
+    width: {
+        type: Integer,
+        required: true,
+        unique: false
+    },
+    height: {
+        type: Integer,
+        required: true,
+        unique: false
+    },
+    glass: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    glass2: {
+        type: String,
+        required: true,
+        unique: false
+    }
+});
 
-export default lodzi;
+const lodzi  = new mongoose.model('Zamer', Schema);
+const lodziExtensive  = new mongoose.model('Zamer', SchemaExtensive);
+
+export default {lodzi, lodziExtensive};
