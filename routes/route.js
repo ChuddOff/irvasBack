845dffe510b1212b3f05c3db97c3303aff2813c5
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-04-08 21:51:11 +0300

    alfa 3.7

--- routes/route.js
+++ routes/route.js
@@ -5,6 +5,7 @@ import Zamer from '../controllers/zamer.js'
 // router.post("/createzamer", Zamer.create)
 router.get("/test", Zamer.test)
 router.post("/go", Zamer.mongopost)
+router.post("/goExtensive", Zamer.mongopostExtensive)
 // router.get("/", Zamer.site)
 
 export default router
